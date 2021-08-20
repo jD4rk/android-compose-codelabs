a3c19072113d648556bd5c50932805d496fc285b
jD4rk <cristian.rosa.78@gmail.com>
2021-08-20 03:19:32 +0200

    [Migration CodeLab] Complete the steps https://developer.android.com/codelabs/jetpack-compose-migration
    Signed-off-by: jD4rk <cristian.rosa.78@gmail.com>

--- MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest.kt
+++ MigrationCodelab/app/src/androidTest/java/com/google/samples/apps/sunflower/GardenActivityTest.kt
@@ -16,19 +16,19 @@
 
 package com.google.samples.apps.sunflower
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 
 class GardenActivityTest {
 
     @Rule @JvmField
-    var activityTestRule = ActivityTestRule(GardenActivity::class.java)
+    var activityTestRule = createAndroidComposeRule(GardenActivity::class.java)
 
     @Test fun clickAddPlant_OpensPlantList() {
         // Given that no Plants are added to the user's garden
